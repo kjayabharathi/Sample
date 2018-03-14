2fc77343985e2f8256313e1758bad1c95ea858fb
parel <parel@Parella-Flex4.mshome.net>
2018-03-14 16:18:24 -0400

    New change

--- src/test/java/com/tng/ohrm/Demo.java
+++ src/test/java/com/tng/ohrm/Demo.java
@@ -5,6 +5,7 @@ public class Demo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("This is new");
+		System.out.println("A new Change");
 	}
 
 }
